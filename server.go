34c66e919f7b2aa749c83023499bed20cea72304
Reuben chong <reubenchongww2512@gmail.com>
2023-11-14 22:42:24 +0800

    serve static htmx and add middlewares

--- server.go
+++ server.go
@@ -5,24 +5,46 @@ import (
 	"portfolio/templates"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Static files
+	e.Static("/assets", "assets")
+
 	// init template renderer
 	templates.NewTemplateRenderer(e, "public/*.html")
 
 	// routes
-	e.GET("/hello", func(c echo.Context) error {
-		res := map[string]interface{} {
-			"Name": "Reuben",
-		}
-		return c.Render(http.StatusOK, "index", res)
+
+	e.GET("/htmx-src", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
 	})
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
+	})
+
+
 
 	e.GET("/test", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "test", nil)
 	})
+
+	e.GET("/get-info", func(c echo.Context) error {
+		res := map[string]interface{}{
+			"Name": "Reuben",
+			"Github": "github.com/reubencww",
+			"Email": "reubenchongww2512@gmail.com",
+		}
+		return c.Render(http.StatusOK, "name_card", res)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
