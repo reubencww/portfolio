79361b17f94412e579032bb8be950293732d4508
Reuben chong <reubenchongww2512@gmail.com>
2023-11-16 11:53:50 +0800

    clean up css files and dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: [
     "./public/**/*.html",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
+    require('flowbite/plugin')
   ],
 }
